9905e77960cf6264c1699ac39e40920541df8eb5
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2025-01-28 10:01:20 -0300

    feat: signin and signup

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,8 @@
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return (
+    <div className="h-screen grid grid-cols-1 md:grid-cols-2">
+      <div className="flex flex-col items-center justify-center">{children}</div>
+      <div className="bg-violet-600 hidden md:block" />
+    </div>
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,8 +25,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="es">
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <main className="w-full bg-[#F3F3F1] h-full min-h-screen">{children}</main>
+
+          <Toaster />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
