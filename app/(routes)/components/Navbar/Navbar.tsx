f202e89c3144c22a8dfd427b6346be194c3e13c2
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2025-01-28 09:31:59 -0300

    fix

--- app/(routes)/components/Navbar/Navbar.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { NavBarProps } from "./Navbar.types";
-
-export const Navbar = (props: NavBarProps) => {
-  const { user } = props;
-  return <div>{user}</div>;
-};
