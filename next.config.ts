e89f712c364be432bcb2312660017141b612bc36
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2025-01-31 07:52:39 -0300

    feat: configure remote image patterns in Next.js for external image support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "vthx1audm4.ufs.sh",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
